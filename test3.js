3d5e108f3b51c198e7d50895189155090b139f7e
Kiwi Chen <kiwi_chen@tutk.com>
2016-10-17 14:59:53 +0800

    test3

---
+++ test3.js
@@ -0,0 +1,8 @@
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return { ...state, visibilityFilter: action.filter }
+    default:
+      return state
+  }
+}
